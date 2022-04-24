786d2b0ba35baed4ed21502bfc10f1a77534955e
Paul McGrath <paul.mcgrath@outlook.com>
2022-04-24 20:50:35 +1000

    2022 Season first commit

--- shiny/SC-Logos/app.R
+++ shiny/SC-Logos/app.R
@@ -28,54 +28,54 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  source('/Users/paulmcgrath/Github/Fantasy-Banter/functions/supercoach_functions.R')
     
-    source('/Users/paulmcgrath/Github/Fantasy-Banter/functions/supercoach_functions.R')
-    
-    settings <- get_settings_data(cid,tkn)
-    
-    sc_league <- get_sc_league(settings$sc_auth, settings$league_id)
-    
-    raw <- sc_league$ladder
-    raw <- lapply(raw, unlist)
-    raw <- bind_rows(lapply(raw, as.data.frame.list))
-    
-    teams<- raw[,grepl('userTeam.',colnames(raw))]
-    colnames(teams) <- sub('userTeam.', '', colnames(teams))
-    teams <- as_tibble(teams[,c(1:12)])
-    
-    output$uiTeamList <- renderUI({
-        selectInput("teamSelector",
-                    "Select Team:",
-                    choices=teams$teamname)
-    })
-    
-    teamIndex <- reactive({
-        req(input$teamSelector) 
-        return(grep(input$teamSelector, teams$teamname)[1])
-    })
-    
-    shirttype <- reactive({return(teams$shirttype[teamIndex()])})
-    shortcolor <- reactive({return(teams$shortcolor[teamIndex()])})
-    basecolor <- reactive({return(teams$basecolor[teamIndex()])})
-    secondcolor <- reactive({return(teams$secondcolor[teamIndex()])})
+  sc <- get_sc(cid,tkn)  
+  sc_league <- get_sc_data(sc$auth, sc$url$league[1])
+  
+  raw <- sc_league$ladder
+  raw <- lapply(raw, unlist)
+  raw <- bind_rows(lapply(raw, as.data.frame.list))
+  
+  teams<- raw[,grepl('userTeam.',colnames(raw))]
+  colnames(teams) <- sub('userTeam.', '', colnames(teams))
+  teams <- as_tibble(teams[,c(1:12)]) %>%
+    mutate(shortcolor = ifelse(shortcolor=='7,0',basecolor,shortcolor))
+  
+  output$uiTeamList <- renderUI({
+      selectInput("teamSelector",
+                  "Select Team:",
+                  choices=teams$teamname)
+  })
+  
+  teamIndex <- reactive({
+      req(input$teamSelector) 
+      return(grep(input$teamSelector, teams$teamname)[1])
+  })
+  
+  shirttype <- reactive({return(teams$shirttype[teamIndex()])})
+  shortcolor <- reactive({return(teams$shortcolor[teamIndex()])})
+  basecolor <- reactive({return(teams$basecolor[teamIndex()])})
+  secondcolor <- reactive({return(teams$secondcolor[teamIndex()])})
 
-    output$uiLogo <- renderUI({
-        get_sc_logo(input$uiLogoSize, shirttype(), shortcolor(), basecolor(), secondcolor())
-    })
-    
-    output$test <- renderHighchart({
-        
-        highchart() %>%
-            hc_chart(type='line') %>%
-            hc_title(text='Ladder Journey') %>%
-            hc_xAxis(opposite=TRUE, title=list(text='Round'), tickInterval=1) %>%
-            hc_yAxis(min=1,max=8, reversed=TRUE, categories=c(NA,1:8),
-                     title=list(text='Ladder Position')) %>%
-            hc_add_series(name="Test",
-                          data=c(1:8),
-                          marker=list(symbol='Paul.png'))
-        
-    })
+  output$uiLogo <- renderUI({
+      get_sc_logo(input$uiLogoSize, shirttype(), shortcolor(), basecolor(), secondcolor())
+  })
+  
+  output$test <- renderHighchart({
+      
+      highchart() %>%
+          hc_chart(type='line') %>%
+          hc_title(text='Ladder Journey') %>%
+          hc_xAxis(opposite=TRUE, title=list(text='Round'), tickInterval=1) %>%
+          hc_yAxis(min=1,max=8, reversed=TRUE, categories=c(NA,1:8),
+                   title=list(text='Ladder Position')) %>%
+          hc_add_series(name="Test",
+                        data=c(1:8),
+                        marker=list(symbol='Paul.png'))
+      
+  })
 }
 
 # Run the application 
